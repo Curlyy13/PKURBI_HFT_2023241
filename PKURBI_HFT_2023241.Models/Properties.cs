a097d1aa22b36a7489d4206e9f2da81773dbc578
Curlyy <Noel13@stud.uni-obuda.hu>
2023-10-24 17:53:22 +0200

    Last modified: 2023.10.21

---
+++ PKURBI_HFT_2023241.Models/Properties.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PKURBI_HFT_2023241.Models
+{
+    [Table("Properties")]
+    public class Properties : Entity
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("PropId", TypeName = "int")]
+        public override int Id { get; set; }
+
+        [Required]
+        [StringLength(150)] 
+        public string PropAdress { get; set; }
+
+        [Required]
+        public double PropValue { get; set; }
+        public double BasicAresa { get; set; }
+    }
+}
---
+++ PKURBI_HFT_2023241.Models/Salepeople.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PKURBI_HFT_2023241.Models
+{
+    [Table("Salespeople")]
+    internal class Salepeople : Entity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public override int Id { get; set; }
+    }
+}
