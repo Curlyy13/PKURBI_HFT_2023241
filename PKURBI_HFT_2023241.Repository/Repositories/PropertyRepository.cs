f437a0b3fb090242ee0ded80e306928cd7962bed
Curlyy13 <noelkovacs14@gmail.com>
2023-11-17 13:26:42 +0100

    Repository-k létrehozva, külön folderbe rakva a Repositories folderen belül, az interfészekek megvalósítják azonban még nincsenek kifejtve a metódusok.

---
+++ PKURBI_HFT_2023241.Repository/Repositories/PropertyRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using PKURBI_HFT_2023241.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PKURBI_HFT_2023241.Repository.Repositories
+{
+    internal class PropertyRepository : IPropertyRepository
+    {
+        public void Create(Property property)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Property Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Property> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Property property)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PKURBI_HFT_2023241.Repository/Repositories/TenantRepository.cs
@@ -0,0 +1,38 @@
+﻿using PKURBI_HFT_2023241.Models;
+using PKURBI_HFT_2023241.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PKURBI_HFT_2023241.Repository.Repositories
+{
+    internal class TenantRepository : ITenantRepository
+    {
+        public void Create(Tenant tenant)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Property Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Tenant> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Tenant tenant)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
