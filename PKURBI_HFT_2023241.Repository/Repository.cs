0dcd309934b69818ab2dfeef1a9f31f2afe5df25
Curlyy13 <noelkovacs14@gmail.com>
2023-11-18 13:38:46 +0100

    Generikus repo osztály és interface létrehozva, a többi törölve.

---
+++ PKURBI_HFT_2023241.Repository/Repository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PKURBI_HFT_2023241.Repository
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        protected AgencyDbContext ctx;
+
+        public Repository(AgencyDbContext ctx)
+        {
+            this.ctx = ctx;
+        }
+        public void Create(T entity)
+        {
+            ctx.Set<T>().Add(entity);
+            ctx.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            ctx.Set<T>().Remove(Read(id));
+            ctx.SaveChanges();
+        }
+
+        public IQueryable<T> ReadAll()
+        {
+            return ctx.Set<T>();
+        }
+
+        public abstract T Read(int id);
+        public abstract void Update(T entity);
+    }
+}
