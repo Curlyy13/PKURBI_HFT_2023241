3d6042fd5eab1797d33ebbf851386ff39f01fdf9
Curlyy13 <noelkovacs14@gmail.com>
2024-03-28 12:30:10 +0100

    WpfClient létrehozva, RestCollection osztály hozzáadva, a UI-on megjelennek a RealEstate-ek.

---
+++ PKURBI_HFT_2023241.WpfClient/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using PKURBI_HFT_2023241.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PKURBI_HFT_2023241.WpfClient
+{
+    public class MainWindowViewModel
+    {
+        public RestCollection<RealEstate> RealEstates { get; set; }
+
+        public MainWindowViewModel()
+        {
+           RealEstates = new RestCollection<RealEstate>("http://localhost:35487/", "RealEstate");
+        }
+    }
+}
