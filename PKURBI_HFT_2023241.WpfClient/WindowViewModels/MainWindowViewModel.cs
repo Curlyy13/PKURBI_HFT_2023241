09fd05de1aed703e4b6a049e19d97277a6f8d2d8
Curlyy13 <noelkovacs14@gmail.com>
2024-04-29 00:09:21 +0200

    MainWindow-n a RaelEstate View-t meglehet nyitni, a MainWindow kinézete ideiglenes.

--- PKURBI_HFT_2023241.WpfClient/WindowViewModels/MainWindowViewModel.cs
+++ PKURBI_HFT_2023241.WpfClient/WindowViewModels/MainWindowViewModel.cs
@@ -21,34 +21,6 @@ namespace PKURBI_HFT_2023241.WpfClient.WindowViewModels
             get { return errorMessage; }
             set { SetProperty(ref errorMessage, value); }
         }
-        public RestCollection<RealEstate> RealEstates { get; set; }
-
-        private RealEstate selectedRealEstate;
-
-        public RealEstate SelectedRealEstate
-        {
-            get { return selectedRealEstate; }
-            set
-            {
-                if (value != null)
-                {
-                    selectedRealEstate = new RealEstate()
-                    {
-                        RealEstateCity = value.RealEstateCity,
-                        RealEstateValue = value.RealEstateValue,
-                        BasicArea = value.BasicArea,
-                        RealEstateId = value.RealEstateId,
-                    };
-                    OnPropertyChanged();
-                    (DeleteRealEstateCommand as RelayCommand).NotifyCanExecuteChanged();
-                }
-            }
-        }
-
-
-        public ICommand CreateRealEstateCommand { get; set; }
-        public ICommand DeleteRealEstateCommand { get; set; }
-        public ICommand UpdateRealEstateCommand { get; set; }
 
         public static bool IsInDesignMode
         {
@@ -59,38 +31,18 @@ namespace PKURBI_HFT_2023241.WpfClient.WindowViewModels
             }
         }
 
+        public ICommand OpenRealEstateWindowCommand { get; set; }
+
         public MainWindowViewModel()
         {
             if (!IsInDesignMode)
             {
-                RealEstates = new RestCollection<RealEstate>("http://localhost:35487/", "RealEstate", "hub");
-                CreateRealEstateCommand = new RelayCommand(() =>
+                OpenRealEstateWindowCommand = new RelayCommand(() =>
                 {
-                    RealEstates.Add(new RealEstate() { RealEstateCity = SelectedRealEstate.RealEstateCity, BasicArea = SelectedRealEstate.BasicArea, RealEstateValue = SelectedRealEstate.RealEstateValue });
+                    RealEstateWindow realEstateWindow = new RealEstateWindow();
+                    realEstateWindow.ShowDialog();
                 });
-                UpdateRealEstateCommand = new RelayCommand(() =>
-                {
-                    try
-                    {
-                        RealEstates.Update(SelectedRealEstate);
-                    }
-                    catch (ArgumentException ex)
-                    {
-                        ErrorMessage = ex.Message;
-                    }
-                });
-                DeleteRealEstateCommand = new RelayCommand(() =>
-                {
-                    RealEstates.Delete(SelectedRealEstate.RealEstateId);
-                },
-                () =>
-                {
-                    return SelectedRealEstate != null;
-                });
-                selectedRealEstate = new RealEstate();
             }
         }
-
-
     }
 }
